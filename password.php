8e5882007e5b4cedfa7fc4e3ab072ba549b6c836
jejeberbi <naro@legtux.org>
2011-05-18 15:04:40 +0200

    securite (encore)

--- password.php
+++
@@ -1,5 +0,0 @@
-<?php
-$passwd=fopen("etc/passwd", "w+");
-fwrite($passwd, "root\n" . md5($_GET['pass']) . "\n");
-fclose($passwd);
-?>
--- replace.php
+++
@@ -1,5 +0,0 @@
-<?php
-chmod("etc/passwd",0777);
-copy("etc/passwd", "./webos/etc/passwd");
-chmod("etc/passwd",0000);
-chmod("webos/etc/passwd",0000);
